599b31386c21bcd8ce68f791d753e34e8075f093
Abdulla Unais <muabdulla@outlook.com>
2017-01-25 01:07:10 +0530

    updated services (unstable)

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {ItemList} from "../item_list/item_list";
+import { DeliveryService } from '../../providers/delivery-service'
 
 @Component({
   selector: 'categories',
@@ -10,20 +11,30 @@ export class Categories {
 
   itemListPage = ItemList;
   filterText:string = '';
-  categories: Array<{title: string, imageUrl: string, description: number, value:string }>;
+  // categories: Array<{title: string, imageUrl: string, description: number, value:string }>;
+  categories: Array<any>;
+  deliveryService : DeliveryService;
   selectedCategory:string;
 
-  constructor(public navCtrl: NavController, private navParams: NavParams) {
-
-    this.categories = [
-      { title: 'Fish', imageUrl: "cover_sl.jpg", description: 3, value: "sl" },
-      { title: 'Meat', imageUrl: "cover_sa.jpg",  description: 12, value: "sa" },
-      { title: 'Fruits', imageUrl: "cover_sa.jpg",  description: 6, value: "qa"  },
-      { title: 'Vegetables', imageUrl: "cover_sa.jpg",  description: 11, value: "uae"  },
-      { title: 'Other', imageUrl: "cover_sl.jpg",  description: 1, value: "us"  }
-    ];
+  constructor(public navCtrl: NavController, private navParams: NavParams, delivery: DeliveryService) {
+    this.deliveryService = delivery;
+    this.initialize();
+    // this.categories = [
+    //   { title: 'Fish', imageUrl: "cover_sl.jpg", description: 3, value: "sl" },
+    //   { title: 'Meat', imageUrl: "cover_sa.jpg",  description: 12, value: "sa" },
+    //   { title: 'Fruits', imageUrl: "cover_sa.jpg",  description: 6, value: "qa"  },
+    //   { title: 'Vegetables', imageUrl: "cover_sa.jpg",  description: 11, value: "uae"  },
+    //   { title: 'Other', imageUrl: "cover_sl.jpg",  description: 1, value: "us"  }
+    // ];
 
     console.log(navParams.data);
+  }
+
+  initialize() {
+    this.deliveryService.getCategories().then((data) =>  {
+      console.info(data);
+      this.categories = data;
+    });
   }
 
 }

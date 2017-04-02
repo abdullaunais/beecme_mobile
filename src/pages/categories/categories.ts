914dac54df55e87cc92d65b454cce03f7d94b9b1
Abdulla Unais <muabdulla@outlook.com>
2017-04-03 04:51:41 +0530

    fixed some issues and change styles in category page

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {ItemList} from "../item_list/item_list";
+import { ItemList } from "../item_list/item_list";
 import { DeliveryService } from '../../providers/delivery-service';
 import { Storage } from '@ionic/storage';
-import {FirstLaunch} from "../first_launch/first_launch";
+import { CartPage } from "../cart/cart";
+
 
 @Component({
   selector: 'categories',
   templateUrl: 'categories.html',
   providers: [DeliveryService],
   // animations: [
- 
- // IMPORT  trigger, state, style, transition, animate, keyframes FROM ANGULAR CORE
+
+  // IMPORT  trigger, state, style, transition, animate, keyframes FROM ANGULAR CORE
   //   //For the background detail
   //   trigger('bounceInBottom', [
   //     state('in', style({
@@ -30,18 +31,29 @@ import {FirstLaunch} from "../first_launch/first_launch";
 export class Categories {
 
   itemListPage = ItemList;
-  filterText:string = '';
+  filterText: string = '';
   selectedCity: any = null;
-  storage : Storage;
+  storage: Storage;
   // categories: Array<{title: string, imageUrl: string, description: number, value:string }>;
   categories: Array<any> = [];
   rows: Array<any> = [];
-  deliveryService : DeliveryService;
-  selectedCategory:string;
+  deliveryService: DeliveryService;
+  selectedCategory: string;
+
+  cartCount: number = 0;
+  isLoading: boolean;
 
   constructor(public navCtrl: NavController, private navParams: NavParams, delivery: DeliveryService, storage: Storage) {
     this.deliveryService = delivery;
     this.storage = storage;
+    this.isLoading = true;
+
+
+    storage.get('delivery.cartCount').then((data) => {
+      if (data) {
+        this.cartCount = data;
+      }
+    });
     // this.categories = [
     //   { title: 'Fish', imageUrl: "cover_sl.jpg", description: 3, value: "sl" },
     //   { title: 'Meat', imageUrl: "cover_sa.jpg",  description: 12, value: "sa" },
@@ -65,7 +77,7 @@ export class Categories {
     console.log(navParams.data);
     if (!navParams.data.locationSet) {
       storage.get('location.city').then((city) => {
-        if(city) {
+        if (city) {
           this.selectedCity = city;
           this.initialize();
         }
@@ -77,12 +89,21 @@ export class Categories {
   }
 
   initialize() {
-    this.deliveryService.getCategories(this.selectedCity.id).then((data) =>  {
+    this.deliveryService.getCategories(this.selectedCity.id).then((data) => {
       let json = JSON.stringify(data);
       this.categories = JSON.parse(json);
       // this.rows = Array.from(Array(Math.ceil(this.categories.length / 2)).keys());
       console.info(this.categories);
+      this.isLoading = false;
     });
+  }
+
+  openCart() {
+    this.navCtrl.push(CartPage, null);
+  }
+
+  openCategory(category) {
+    this.navCtrl.push(this.itemListPage, category, {animate: true, direction: "forward"})
   }
 
 }

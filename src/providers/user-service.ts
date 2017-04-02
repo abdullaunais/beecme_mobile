5fd0c9eb437a9a987685473e1d4165d71b718081
Abdulla Unais <muabdulla@outlook.com>
2017-04-03 04:59:50 +0530

    added registration and auth services

--- src/providers/user-service.ts
+++ src/providers/user-service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Config } from './config';
 import 'rxjs/add/operator/map';
 
@@ -18,10 +18,61 @@ export class UserService {
   data: any;
   serviceRootUrl : string;
 
+  REGISTER_URL = "/register";  // post
+  GET_USER_URL = "/users/"; // +username, get
+  AUTHENTICATE_LOGIN = "/auth" // post
 
   constructor(public httpService: Http, public config:Config) {
     this.http = httpService;
     this.data = null;
-    this.serviceRootUrl = config.serverUrl + "/delivery/users";
+    this.serviceRootUrl = config.serverUrl + "/delivery";
+  }
+
+  registerUser(user) {
+    let body = JSON.stringify(user);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+
+    let requestUrl : string = this.serviceRootUrl + this.REGISTER_URL;
+    return new Promise(resolve => {
+      this.http.post(requestUrl, body, options)
+          .subscribe(data => {
+            resolve(data.json());
+          }, err => { 
+            resolve(err.json());
+          });
+    });
+  }
+
+  getUserDetails(email) {
+    let requestUrl : string = this.serviceRootUrl + this.GET_USER_URL + "/" + email;
+    return new Promise(resolve => {
+      this.http.get(requestUrl)
+          .subscribe(data => {
+            resolve(data.json());
+          });
+    });
+  }
+
+  authenticate(email, password) {
+    let credentials = {
+      username: email,
+      password: password
+    };
+    let body = JSON.stringify(credentials);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+
+    let requestUrl : string = this.serviceRootUrl + this.AUTHENTICATE_LOGIN;
+    return new Promise(resolve => {
+      this.http.post(requestUrl, body, options)
+          .subscribe(data => {
+            resolve(data);
+          }, err => {
+            resolve(err);
+          });
+    });
   }
 }

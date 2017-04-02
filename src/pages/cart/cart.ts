dfdef07048008f4fd90ea3daad2b1fb46900f2e4
Abdulla Unais <muabdulla@outlook.com>
2017-04-03 04:52:03 +0530

    loader added to cart page

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import { DetailsPage } from "../details/details";
+import { DeliverySchedulePage } from "../delivery-schedule/delivery-schedule";
+import { UserLoginPage } from "../user-login/user-login";
 
 /*
   Generated class for the Cart page.
@@ -18,10 +21,12 @@ export class CartPage {
   storage : Storage;
 
   cartIsEmpty : boolean = false;
+  isLoading: boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, storage : Storage) {
     this.cartItems = [];
     this.storage = storage;
+    this.isLoading = true;
 
     storage.get('delivery.cart').then((cart) => {
       console.info("CartItems --> ", cart);
@@ -36,6 +41,32 @@ export class CartPage {
       } else {
         this.cartItems = [];
         this.cartIsEmpty = true;
+      }
+      this.isLoading = false;
+    });
+  }
+
+  removeItem(item) {
+    this.cartItems.splice(
+      this.cartItems.findIndex(
+        (elem) => elem.itemCode === item.itemCode
+      ), 1
+    );
+    this.storage.set('delivery.cart', this.cartItems);
+  }
+
+  viewItem(item) {
+    this.navCtrl.push(DetailsPage, item);
+  }
+
+  checkout() {
+    this.storage.get('user.login').then((auth) => {
+      if(auth) {
+        this.navCtrl.push(DeliverySchedulePage, this.cartItems);
+      } else {
+        // DeliverySchedulePage - testing
+        // UserLoginPage - original
+        this.navCtrl.push(UserLoginPage, "redirect-deliveryschedule"); 
       }
     });
   }

757390d7481aac93c74d08932040fa7792d20022
Abdulla Unais <muabdulla@outlook.com>
2017-04-12 04:11:19 +0530

    delivery services change

--- src/providers/delivery-service.ts
+++ src/providers/delivery-service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Config } from './config';
 import 'rxjs/add/operator/map';
@@ -16,7 +16,7 @@ export class DeliveryService {
 
   http: Http;
   data: any;
-  serviceRootUrl : string;
+  serviceRootUrl: string;
 
   LOCATION_URL = "/locations";
   CATEGORIES_URL = "/categories";
@@ -24,52 +24,51 @@ export class DeliveryService {
   SCHEDULES_URL = "/schedules";
   ADD_ORDER = "/carts"
 
-  constructor(public httpService: Http, public config:Config) {
-    console.log('Hello DeliveryService Provider');
+  constructor(public httpService: Http, public config: Config) {
     this.http = httpService;
     this.data = null;
     this.serviceRootUrl = config.serverUrl + "/delivery";
   }
 
   getLocation(type, value, start, offset) {
-    let queryParams = "?type="+type+"&value="+value+"&start="+start+"&offset="+offset;
-    let requestUrl : string = this.serviceRootUrl + this.LOCATION_URL + queryParams;
-    return new Promise(resolve => {
+    let queryParams = "?type=" + type + "&value=" + value + "&start=" + start + "&offset=" + offset;
+    let requestUrl: string = this.serviceRootUrl + this.LOCATION_URL + queryParams;
+    return new Promise((resolve) => {
       this.http.get(requestUrl)
-          .subscribe(data => {
-            resolve(data.json());
-          });
-    });
+        .subscribe(data => {
+          resolve(data.json());
+        });
+    },);
   }
 
   getCategories(cityId) {
-    let requestUrl : string = this.serviceRootUrl + this.CATEGORIES_URL + "/" + cityId;
+    let requestUrl: string = this.serviceRootUrl + this.CATEGORIES_URL + "/" + cityId;
     return new Promise(resolve => {
       this.http.get(requestUrl)
-          .subscribe(data => {
-            resolve(data.json());
-          });
+        .subscribe(data => {
+          resolve(data.json());
+        });
     });
   }
 
   getItemByCategory(category, start, offset) {
-    let queryParams = "?type=11&value="+category+"&start="+start+"&offset="+ offset;
-    let requestUrl :string = this.serviceRootUrl + this.ITEM_URL + queryParams;
+    let queryParams = "?type=11&value=" + category + "&start=" + start + "&offset=" + offset;
+    let requestUrl: string = this.serviceRootUrl + this.ITEM_URL + queryParams;
     return new Promise(resolve => {
       this.http.get(requestUrl)
-          .subscribe(data => {
-            resolve(data.json());
-          });
+        .subscribe(data => {
+          resolve(data.json());
+        });
     });
   }
 
-    getSchedules(cityId) {
-    let requestUrl : string = this.serviceRootUrl + this.SCHEDULES_URL + "?city=" + cityId;
+  getSchedules(cityId) {
+    let requestUrl: string = this.serviceRootUrl + this.SCHEDULES_URL + "?city=" + cityId;
     return new Promise(resolve => {
       this.http.get(requestUrl)
-          .subscribe(data => {
-            resolve(data.json());
-          });
+        .subscribe(data => {
+          resolve(data.json());
+        });
     });
   }
 
@@ -80,14 +79,14 @@ export class DeliveryService {
     headers.append('Authorization', authToken)
     let options = new RequestOptions({ headers: headers });
 
-    let requestUrl : string = this.serviceRootUrl + this.ADD_ORDER;
+    let requestUrl: string = this.serviceRootUrl + this.ADD_ORDER;
     return new Promise(resolve => {
       this.http.post(requestUrl, body, options)
-          .subscribe(data => { 
-            resolve(data);
-          }, err => {
-            resolve(err);
-          });
+        .subscribe(data => {
+          resolve(data);
+        }, err => {
+          resolve(err);
+        });
     });
   }
 }

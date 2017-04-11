0b5856a7b2707e61129ec1ac0cc61d413c0488b8
Abdulla Unais <muabdulla@outlook.com>
2017-04-12 04:14:39 +0530

    improved settings

--- src/pages/app-settings/app-settings.ts
+++ src/pages/app-settings/app-settings.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController, LoadingController, ViewController, AlertController } from 'ionic-angular';
 import { Storage } from "@ionic/storage";
 import { Categories } from "../categories/categories";
 import { UserLoginPage } from "../user-login/user-login";
+import { Variables } from "../../providers/variables";
+import { ChangeLocation } from "../change-location/change-location";
 
 /*
   Generated class for the AppSettings page.
@@ -19,15 +21,22 @@ export class AppSettingsPage {
   logTitle: string = "Login";
   isLogin: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, storage: Storage) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    storage: Storage,
+    private variables: Variables,
+    public modalCtrl: ModalController,
+    private alertCtrl: AlertController
+  ) {
     this.storage = storage;
+    this.variables.login.subscribe(value => this.isLogin = value);
     storage.get("user.login").then((response) => {
-      if(response) {
-        this.logTitle = "Logout"
-        this.isLogin = true;
+      this.variables.setLogin(response);
+      if (response) {
+        this.logTitle = "Logout";
       } else {
         this.logTitle = "Login"
-        this.isLogin = false;
       }
     });
   }
@@ -37,17 +46,38 @@ export class AppSettingsPage {
   }
 
   loginOrLogout() {
-    if(this.isLogin) {
+    if (this.isLogin) {
       this.storage.set("user.login", false).then((res1) => {
-      this.storage.set("user.data", {}).then((res2) => {
-        this.navCtrl.setRoot(Categories, null);
+        this.storage.set("user.data", {}).then((res2) => {
+          this.variables.setLogin(false);
+          Variables.user = {};
+          this.navCtrl.setRoot(Categories, null);
+        });
       });
-    });
     } else {
       this.navCtrl.setRoot(UserLoginPage, null);
     }
-    
-    
   }
 
+  presentChangeLocationModal() {
+    let changeLocationModal = this.modalCtrl.create(ChangeLocation);
+    changeLocationModal.present();
+    changeLocationModal.onDidDismiss((data) => {
+      if (data.success) {
+        let alert = this.alertCtrl.create({
+          title: 'Success',
+          message: 'Location Changed. Your cart and the login data is reset.',
+          buttons: [
+            {
+              text: 'OK',
+              handler: () => {
+                //ignore
+              }
+            }
+          ]
+        });
+        alert.present();
+      }
+    });
+  }
 }

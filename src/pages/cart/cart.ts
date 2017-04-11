c111eb7b01c1f37c690219bb7095463eb7792a90
Abdulla Unais <muabdulla@outlook.com>
2017-04-12 04:07:24 +0530

    bug fixes and improvements in cart page

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { DetailsPage } from "../details/details";
 import { DeliverySchedulePage } from "../delivery-schedule/delivery-schedule";
 import { UserLoginPage } from "../user-login/user-login";
+import { Variables } from "../../providers/variables";
 
 /*
   Generated class for the Cart page.
@@ -18,22 +19,34 @@ import { UserLoginPage } from "../user-login/user-login";
 export class CartPage {
 
   cartItems: Array<any>;
-  storage : Storage;
+  storage: Storage;
 
-  cartIsEmpty : boolean = false;
+  cartIsEmpty: boolean = false;
   isLoading: boolean;
+  totalAmount: number = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, storage : Storage) {
+  checkoutComment: string = "";
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    storage: Storage,
+    private variables: Variables,
+    private alertCtrl: AlertController
+  ) {
     this.cartItems = [];
     this.storage = storage;
     this.isLoading = true;
 
     storage.get('delivery.cart').then((cart) => {
       console.info("CartItems --> ", cart);
-      if(cart) {
-        if(cart.length > 0) {
+      if (cart) {
+        if (cart.length > 0) {
           this.cartItems = cart;
           this.cartIsEmpty = false;
+          this.cartItems.forEach((item) => {
+            this.totalAmount = this.totalAmount + (item.price * item.quantity);
+          })
         } else {
           this.cartItems = [];
           this.cartIsEmpty = true;
@@ -52,7 +65,13 @@ export class CartPage {
         (elem) => elem.itemCode === item.itemCode
       ), 1
     );
+    this.totalAmount = 0;
+    this.cartItems.forEach((item) => {
+      this.totalAmount = this.totalAmount + (item.price * item.quantity);
+    })
     this.storage.set('delivery.cart', this.cartItems);
+    this.storage.set('delivery.cartCount', this.cartItems.length);
+    this.variables.setCartCount(this.cartItems.length);
   }
 
   viewItem(item) {
@@ -60,19 +79,34 @@ export class CartPage {
   }
 
   checkout() {
-    this.storage.get('user.login').then((auth) => {
-      if(auth) {
-        this.navCtrl.push(DeliverySchedulePage, this.cartItems);
-      } else {
-        // DeliverySchedulePage - testing
-        // UserLoginPage - original
-        this.navCtrl.push(UserLoginPage, "redirect-deliveryschedule"); 
-      }
+    let prompt = this.alertCtrl.create({
+      title: 'Checkout Comment',
+      message: "",
+      cssClass: 'prompt-ui-theme',
+      inputs: [
+        { name: 'comment', placeholder: 'Comment' },
+      ],
+      buttons: [
+        { text: 'Cancel', handler: data => { } },
+        {
+          text: 'Save',
+          handler: data => {
+            this.checkoutComment = data.comment;
+            Variables.checkoutComment = this.checkoutComment;
+
+            this.storage.get('user.login').then((auth) => {
+              if (auth) {
+                this.navCtrl.push(DeliverySchedulePage, this.checkoutComment);
+              } else {
+                // DeliverySchedulePage - testing
+                // UserLoginPage - original
+                this.navCtrl.push(UserLoginPage, "redirect-deliveryschedule");
+              }
+            });
+          }
+        }
+      ]
     });
+    prompt.present();
   }
-
-  // ionViewDidLoad() {
-  //   console.log('ionViewDidLoad CartPage');
-  // }
-
 }

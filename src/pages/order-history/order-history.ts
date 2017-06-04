3603a5d67d6330365f2fe0348c93dfb0a8a0cdbe
Abdulla Unais <muabdulla@outlook.com>
2017-06-05 03:52:16 +0530

    my orders ui

--- src/pages/order-history/order-history.ts
+++ src/pages/order-history/order-history.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, IonicPage } from 'ionic-angular';
+import { NavController, NavParams, IonicPage, ActionSheetController } from 'ionic-angular';
 
 /*
   Generated class for the OrderHistory page.
@@ -14,10 +14,41 @@ import { NavController, NavParams, IonicPage } from 'ionic-angular';
 })
 export class OrderHistoryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public actionSheetCtrl: ActionSheetController
+  ) {
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderHistoryPage');
+  }
+
+  getActions(order: any) {
+    let actionSheet = this.actionSheetCtrl.create({
+      // title: 'Select Quantity',
+      buttons: [
+        {
+          text: 'Send Feedback',
+          icon: 'star',
+          cssClass: 'action-feedback-btn',
+          handler: () => {
+            console.log('Feedback Clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close-circle',
+          cssClass: 'action-cancel-btn',
+          role: 'cancel',
+          handler: () => {
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }

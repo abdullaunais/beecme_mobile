d0781072634bc6560554068da46d1d07b5de91f4
Abdulla Unais <muabdulla@outlook.com>
2017-06-21 01:48:23 +0530

    complete: user profile, parallax header

--- src/pages/user-profile/user-profile.module.ts
+++ src/pages/user-profile/user-profile.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UserProfilePage } from "./user-profile";
+import { ParallaxHeader } from "../../directives/parallax-header/parallax-header";
 
 @NgModule({
   declarations: [
     UserProfilePage,
+    ParallaxHeader
   ],
   imports: [
     IonicPageModule.forChild(UserProfilePage),
   ],
   exports: [
     UserProfilePage
+    
   ]
 })
 export class UserProfilePageModule {}
--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController, IonicPage } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, NavParams, AlertController, IonicPage, LoadingController, ToastController, ModalController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation';
+import { UserService } from "../../providers/user-service";
 
+declare var google;
 /*
   Generated class for the UserProfile page.
 
@@ -11,40 +14,173 @@ import { Storage } from '@ionic/storage';
 @IonicPage()
 @Component({
   selector: 'page-user-profile',
-  templateUrl: 'user-profile.html'
+  templateUrl: 'user-profile.html',
+  providers: [UserService, Geolocation]
 })
 export class UserProfilePage {
+  userAddressElement: any;
+  loading: any;
+  offsetHeight: number;
   user: any = {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, storage: Storage) {
+  hideNav: boolean = false;
+  isLoading: boolean = true;
 
-    storage.get("user.login").then((login) => {
-      if(login) {
-        storage.get("user.data").then((userData) => {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  marker: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    public modalCtrl: ModalController,
+    private storage: Storage,
+    private geolocation: Geolocation,
+    private userService: UserService
+  ) { }
+
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(this.user.latitude, this.user.longitude);
+    let mapOptions = {
+      center: latLng,
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map
+    });
+  }
+
+  updateLocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.showLoading("Updating...");
+      // resp.coords.latitude
+      // resp.coords.longitude
+      // console.info("Latitude -- ", resp.coords.latitude);
+      // console.info("Longitude -- ", resp.coords.longitude);
+      delete this.user.password;
+      this.user.latitude = resp.coords.latitude;
+      this.user.longitude = resp.coords.longitude;
+
+      this.userService.updateUser(this.user).then((updateResponse) => {
+        if (updateResponse.code === 1) {
+          this.storage.set('user.data', this.user);
+          let alert = this.alertCtrl.create({
+            title: "Location Updated",
+            message: 'New Delivery Location is updated.',
+            cssClass: 'alert-style',
+            buttons: [
+              {
+                text: 'OK',
+                cssClass: 'alert-button-success',
+                handler: () => {
+                  this.hideLoading();
+                  this.loadMap();
+                }
+              }
+            ]
+          });
+          alert.present();
+        } else {
+          this.hideLoading();
+          this.presentToast("Error in location update", 2000);
+        }
+      }).catch(err => {
+        console.error(err);
+        this.hideLoading();
+      });
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+      this.presentToast("Error getting location", 2000);
+    });
+  }
+
+  ngOnInit() {
+    this.storage.get("user.login").then((login) => {
+      if (login) {
+        this.storage.get("user.data").then((userData) => {
           this.user = userData;
+          this.userAddressElement = this.user.address.replace(/(?:\r\n|\r|\n)/g, '<br />');
+          console.log(this.user);
+          this.isLoading = false;
         })
       } else {
-        // redirect to login
+        return;
       }
     });
-    // let alert = this.alertCtrl.create({
-    //   title: 'Message',
-    //   message: 'Still in Development. Will be available soon.',
-    //   cssClass: 'alert-ui-theme-success',
-    //   buttons: [
-    //     {
-    //       text: 'OK',
-    //       handler: () => {
-    //         //ignore
-    //       }
-    //     }
-    //   ]
-    // });
-    // alert.present();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserProfilePage');
+  checkScroll(event) {
+    let yOffset = document.getElementById('profile-content').offsetTop;
+    if (event.scrollTop > yOffset - this.offsetHeight) {
+      document.getElementById('header-content').classList.remove("profile-header");
+    } else {
+      document.getElementById('header-content').classList.add("profile-header");
+    }
   }
 
+  choosePhoto() {
+    
+  }
+
+  presentUpdateModal(property: string) {
+    let updateProfileModal = this.modalCtrl.create('UpdateProfile', {property: property});
+    updateProfileModal.present();
+    updateProfileModal.onDidDismiss((data) => {
+      if (data.success) {
+        // let alert = this.alertCtrl.create({
+        //   title: 'Success',
+        //   cssClass: 'alert-style',
+        //   message: 'Location Changed. Your cart and the login data is reset.',
+        //   buttons: [
+        //     {
+        //       text: 'OK',
+        //       cssClass: 'alert-button-success',
+        //       handler: () => {
+        //         //ignore
+        //       }
+        //     }
+        //   ]
+        // });
+        // alert.present();
+      }
+    });
+  }
+
+
+  ionViewDidEnter() {
+    this.offsetHeight = document.getElementById('nav-content').offsetHeight;
+    setTimeout(() => {
+      this.loadMap();
+    }, 500);
+  }
+
+  showLoading(content) {
+    this.loading = this.loadingCtrl.create({
+      content: content,
+    });
+    this.loading.present();
+  }
+
+  hideLoading() {
+    this.loading.dismiss();
+  }
+
+  presentToast(message, duration) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      showCloseButton: true,
+      closeButtonText: 'OK',
+      duration: duration,
+      position: 'top'
+    });
+    toast.present();
+  }
 }

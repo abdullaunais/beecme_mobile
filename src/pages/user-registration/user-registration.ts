5e97704961f0d100b5dd67337c5fc715e0a71343
Abdulla Unais <muabdulla@outlook.com>
2017-07-11 03:07:44 +0530

    change: address/location no longer requested at registration

--- src/pages/user-registration/user-registration.ts
+++ src/pages/user-registration/user-registration.ts
@@ -2,10 +2,8 @@ import { Component, ViewChildren, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams, AlertController, LoadingController, ToastController, IonicPage } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { UserService } from "../../providers/user-service";
-import { Geolocation } from '@ionic-native/geolocation';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
-declare var google;
 /*
   Generated class for the UserRegistration page.
 
@@ -16,20 +14,13 @@ declare var google;
 @Component({
   selector: 'page-user-registration',
   templateUrl: 'user-registration.html',
-  providers: [UserService, Geolocation, ReactiveFormsModule]
+  providers: [UserService, ReactiveFormsModule]
 })
 export class UserRegistrationPage {
-  countryCode: string = "sa";
-
-  latitude: any;
-  longitude: any;
   username: string;
   email: string;
   phone: string;
   password: any;
-  address1: string;
-  address2: string = "";
-  address3: string = "";
 
   city: any;
   province: any;
@@ -40,12 +31,7 @@ export class UserRegistrationPage {
   registerPressed: boolean = false;
   errorColored: boolean = false;
   validationArray: Array<any> = [];
-  map: any;
-  marker: any;
 
-  isLocated: boolean = false;
-
-  @ViewChild('map') mapElement: ElementRef;
   @ViewChildren('forminput') formInputs;
   @ViewChildren('formitem') formItems;
   getItems = () => {
@@ -56,9 +42,6 @@ export class UserRegistrationPage {
     formEmail: ["", [Validators.required, Validators.minLength(6), Validators.email]],
     formPhone: ["", [Validators.required, Validators.minLength(9)]],
     formPassword: ["", [Validators.required, Validators.minLength(6)]],
-    formAddress1: ["", [Validators.required, Validators.minLength(2)]],
-    formAddress2: ["", []],
-    formAddress3: ["", []],
   });
   constructor(
     public navCtrl: NavController,
@@ -67,45 +50,18 @@ export class UserRegistrationPage {
     private userService: UserService,
     private alertCtrl: AlertController,
     public loadingCtrl: LoadingController,
-    private geolocation: Geolocation,
     public fb: FormBuilder,
     public toastCtrl: ToastController
   ) {
     this.redirectString = navParams.data;
     storage.get('location.city').then((city) => {
-      if (city) {
-        this.city = city;
-        storage.get('location.province').then((province) => {
-          if (province) {
-            this.province = province;
-            storage.get('location.country').then((country) => {
-              if (country) {
-                this.country = country;
-              }
-            });
-          }
+      this.city = city;
+      storage.get('location.province').then((province) => {
+        this.province = province;
+        storage.get('location.country').then((country) => {
+          this.country = country;
         });
-      }
-    });
-
-    this.updateLocation();
-  }
-
-  updateLocation() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      // resp.coords.latitude
-      // resp.coords.longitude
-      // console.info("Latitude -- ", resp.coords.latitude);
-      this.latitude = resp.coords.latitude;
-      // console.info("Longitude -- ", resp.coords.longitude);
-      this.longitude = resp.coords.longitude;
-      this.isLocated = true;
-      setTimeout(() => {
-        this.loadMap();
-      }, 500);
-    }).catch((error) => {
-      this.isLocated = false;
-      console.log('Error getting location', error);
+      });
     });
   }
 
@@ -114,136 +70,98 @@ export class UserRegistrationPage {
   }
 
   registerUser() {
-    if (!this.isLocated) {
-      let locationAlert = this.alertCtrl.create({
-        title: 'Location Update Failed',
-        message: 'We could not detect your current location. Please enable your location and grant permissions to the app when requested.',
-        cssClass: 'alert-style',
-        buttons: [
-          {
-            text: 'OK',
-            cssClass: 'alert-button-danger',
-            handler: data => {
-              this.updateLocation();
-            }
+    let confirmAlert = this.alertCtrl.create({
+      title: 'Confim Signup',
+      message: 'Do you wish to proceed signup?',
+      cssClass: 'alert-style',
+      buttons: [
+        {
+          text: 'Cancel',
+          cssClass: 'alert-button-danger-plain',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
           }
-        ]
-      });
-      locationAlert.present();
-    } else {
-      let confirmAlert = this.alertCtrl.create({
-        title: 'Confim Signup',
-        message: 'Do you wish to proceed signup?',
-        cssClass: 'alert-style',
-        buttons: [
-          {
-            text: 'Cancel',
-            cssClass: 'alert-button-danger-plain',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Yes',
-            cssClass: 'alert-button-primary',
-            handler: () => {
-              this.showLoading("Registering...")
-              let user = {
-                "username": this.username,
-                "email": this.email,
-                "password": this.password,
-                "phone": this.phone,
-                "address": this.address1 + "\n" + this.address2 + "\n" + this.address3,
-                "country": this.country.id,
-                "province": this.province.id,
-                "city": this.city.id,
-                "longitude": this.longitude,
-                "latitude": this.latitude,
-                "type": "1",
-                "notificationSend": "0"
-              };
-              this.userService.registerUser(user).then((data) => {
-                let json = JSON.stringify(data);
-                let response = JSON.parse(json);
-                // this.rows = Array.from(Array(Math.ceil(this.categories.length / 2)).keys());
-                // console.info(response);
-                this.hideLoading();
+        },
+        {
+          text: 'Yes',
+          cssClass: 'alert-button-primary',
+          handler: () => {
+            this.showLoading("Registering...")
+            let user = {
+              "username": this.username,
+              "email": this.email,
+              "password": this.password,
+              "phone": this.phone,
+              "country": this.country.id,
+              "province": this.province.id,
+              "city": this.city.id,
+              "type": "1",
+              "notificationSend": "0"
+            };
+            this.userService.registerUser(user).then((data) => {
+              let json = JSON.stringify(data);
+              let response = JSON.parse(json);
+              this.hideLoading();
 
-                if (response.code === 1) {
-                  // register success
+              if (response.code === 1) {
+                // register success
+                let prompt = this.alertCtrl.create({
+                  title: 'Registration Success',
+                  message: "Please Login with the details provided.",
+                  cssClass: 'alert-style',
+                  buttons: [
+                    {
+                      text: 'OK',
+                      cssClass: 'alert-button-success',
+                      handler: data => {
+                        if (this.redirectString === "redirect-deliveryschedule") {
+                          this.navCtrl.push('UserLoginPage', "redirect-deliveryschedule");
+                        } else {
+                          this.navCtrl.push('UserLoginPage', null);
+
+                        }
+                      }
+                    }
+                  ]
+                });
+                prompt.present();
+              } else if (response.code < 0) {
+                if (response.message === "Request failed. Code already exists") {
                   let prompt = this.alertCtrl.create({
-                    title: 'Registration Success',
-                    message: "Please Login with the details provided.",
+                    title: 'Already Registered',
+                    message: "This email is already been registered. Login with your account instead?",
                     cssClass: 'alert-style',
                     buttons: [
                       {
-                        text: 'OK',
+                        text: 'Cancel',
+                        cssClass: 'alert-button-danger',
+                        handler: data => {
+                        }
+                      },
+                      {
+                        text: 'Login',
                         cssClass: 'alert-button-success',
                         handler: data => {
+                          let navTransition = prompt.dismiss();
                           if (this.redirectString === "redirect-deliveryschedule") {
-                            this.navCtrl.push('UserLoginPage', "redirect-deliveryschedule");
+                            navTransition.then(() => {
+                              this.navCtrl.push('UserLoginPage', "redirect-deliveryschedule");
+                            });
                           } else {
-                            this.navCtrl.push('UserLoginPage', null);
-
+                            navTransition.then(() => {
+                              this.navCtrl.push('UserLoginPage', null);
+                            });
                           }
                         }
                       }
                     ]
                   });
                   prompt.present();
-                } else if (response.code < 0) {
-                  if (response.message === "Request failed. Code already exists") {
-                    let prompt = this.alertCtrl.create({
-                      title: 'Already Registered',
-                      message: "This email is already been registered. Login with your account instead?",
-                      cssClass: 'alert-style',
-                      buttons: [
-                        {
-                          text: 'Cancel',
-                          cssClass: 'alert-button-danger',
-                          handler: data => {
-                          }
-                        },
-                        {
-                          text: 'Login',
-                          cssClass: 'alert-button-success',
-                          handler: data => {
-                            let navTransition = prompt.dismiss();
-                            if (this.redirectString === "redirect-deliveryschedule") {
-                              navTransition.then(() => {
-                                this.navCtrl.push('UserLoginPage', "redirect-deliveryschedule");
-                              });
-                            } else {
-                              navTransition.then(() => {
-                                this.navCtrl.push('UserLoginPage', null);
-                              });
-                            }
-                          }
-                        }
-                      ]
-                    });
-                    prompt.present();
-                  } else {
-                    let prompt = this.alertCtrl.create({
-                      title: 'Registeration Failed',
-                      message: response.message,
-                      cssClass: 'alert-style',
-                      buttons: [
-                        {
-                          text: 'OK',
-                          cssClass: 'alert-button-danger',
-                          handler: data => {
-                          }
-                        }
-                      ]
-                    });
-                    prompt.present();
-                  }
                 } else {
                   let prompt = this.alertCtrl.create({
-                    title: 'Registration Failed',
-                    message: "Something went wrong when registering your account.",
+                    title: 'Registeration Failed',
+                    message: response.message,
                     cssClass: 'alert-style',
                     buttons: [
                       {
@@ -256,31 +174,31 @@ export class UserRegistrationPage {
                   });
                   prompt.present();
                 }
-              }, (err) => {
-                this.hideLoading();
-                console.log("Error - ", err);
-              });
-            }
+              } else {
+                let prompt = this.alertCtrl.create({
+                  title: 'Registration Failed',
+                  message: "Something went wrong when registering your account.",
+                  cssClass: 'alert-style',
+                  buttons: [
+                    {
+                      text: 'OK',
+                      cssClass: 'alert-button-danger',
+                      handler: data => {
+                      }
+                    }
+                  ]
+                });
+                prompt.present();
+              }
+            }, (err) => {
+              this.hideLoading();
+              console.log("Error - ", err);
+            });
           }
-        ]
-      });
-      confirmAlert.present();
-    }
-  }
-
-  loadMap() {
-    let latLng = new google.maps.LatLng(this.latitude, this.longitude);
-    let mapOptions = {
-      center: latLng,
-      zoom: 18,
-      disableDefaultUI: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-    this.marker = new google.maps.Marker({
-      position: latLng,
-      map: this.map
+        }
+      ]
     });
+    confirmAlert.present();
   }
 
   registerClick() {
@@ -294,10 +212,6 @@ export class UserRegistrationPage {
   }
 
   validate() {
-    // console.log(event);
-    // console.log(this.registerForm.value);
-    // console.log(this.registerForm);
-
     let isValid: boolean = true;
     let message: string = "";
     let formIndex: number = 0;
@@ -366,20 +280,6 @@ export class UserRegistrationPage {
       }
     }
 
-    if (this.registerForm.controls.formAddress1.errors) {
-      if (this.registerForm.controls.formAddress1.errors.required) {
-        isValid = false;
-        message = "Address is required";
-        formIndex = 4;
-        this.validationArray.push({ message: message, valid: isValid, index: formIndex });
-      } else if (this.registerForm.controls.formAddress1.errors.minlength) {
-        isValid = false;
-        message = "Address is invalid";
-        formIndex = 4;
-        this.validationArray.push({ message: message, valid: isValid, index: formIndex });
-      }
-    }
-
     // console.log(this.registerPressed);
     if (!isValid) {
       if (this.registerPressed) {
@@ -404,18 +304,11 @@ export class UserRegistrationPage {
       this.email = this.registerForm.value.formEmail;
       this.phone = this.registerForm.value.formPhone;
       this.password = this.registerForm.value.formPassword;
-      this.address1 = this.registerForm.value.formAddress1;
-      this.address2 = this.registerForm.value.formAddress2;
-      this.address3 = this.registerForm.value.formAddress3;
       if (this.registerPressed) {
         this.registerUser();
       }
       return;
     }
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserRegistrationPage');
   }
 
   showLoading(content) {

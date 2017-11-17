f96984a5b345ca629416e5d9bb61c16e57e75fbd
abdullaunais <muabdulla@outlook.com>
2017-11-17 06:40:26 +0530

    change to https server url

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -9,14 +9,11 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class Config {
-  private readonly serverHost = '54.254.246.56'; // local: 192.168.0.102 // aws: 54.254.246.56
-  private readonly serverPort = '8080';
-  private readonly serverPath = 'delivery';
-  private serverUrl: string;
+  private readonly serverUrl = `https://beecme.com/delivery`;
+  //  private readonly serverUrl: string = // local: 192.168.0.102 // aws: 54.254.246.56
   constructor() {
-    this.serverUrl = `http://${this.serverHost}:${this.serverPort}/${this.serverPath}`;
+  
   }
-
   getServerUrl() {
     return this.serverUrl;
   }
